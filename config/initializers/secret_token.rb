1fde76e01bd391e6c3ac855aae5fc1b3c0ea8c94
Jordan Nemrow <nemrowj@gmail.com>
2013-08-01 08:27:26 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jamify::Application.config.secret_token = '820488762bf62103befbdeca1db171185b3e50ef78209fd73af8f2ddc3240bbceb9a37f05af7a93e02c161e1d52f0182753ad936a3baa77012980e8837ba970f'
