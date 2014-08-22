34034095cf1867e393054fb211ef814266d72294
Jordan Nemrow <nemrowj@gmail.com>
2014-08-22 11:57:37 -0700

    Add full mix_down data to api

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,9 @@
 class SearchController < ApplicationController
   def search_all
     mix_downs = MixDownSearch.new(params[:keywords]).results
-    render :json => {:response => true, :mix_downs => mix_downs}
+    mix_downs_data_as_json = mix_downs.map do |mix_down|
+      mix_down.build_data_hash
+    end
+    render :json => {:response => true, :mix_downs => mix_downs_data_as_json}
   end
 end
